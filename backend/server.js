916a2c6c71a6b01f33c610d14d80d6027b587401
Deepalijichkar <deepaliJichkar@gmail.com>
2026-05-21 17:50:35 +0530

    Fix Vercel serverless

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 5000,
-  family: 4
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log('MongoDB error:', err));
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  isConnected = true;
+  console.log('MongoDB connected');
+};
+
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
+});
 
 app.use('/api/bugs', bugRoutes);
 app.use('/api/auth', authRoutes);
@@ -28,5 +35,4 @@ app.get('/', (req, res) => {
   res.send('Bug Tracker API is running');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
